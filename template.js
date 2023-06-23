9e9744b6dac96ac4ba3db5908cef139fc9484822
Batu22 <batuhantemel14@gmail.com>
2023-06-23 17:04:08 +0300

    Search, Dark mode, Header, Footer

--- template.js
+++ template.js
@@ -1,1 +1,25 @@
-//alert("Deneme");
+// Dark Mode
+let darkModu = () => {       // => Arrow function() 
+    alert("Dark Mode");
+    window.document.classList.toggle("dark_mode");
+}
+
+//input search  // Jquery'den yararlandik...
+$(document).ready(function(){
+const searchApi = ["Ankara","Izmir","Istanbul","Antalya","Bursa","Balikesir","Aydin","Kutahya"];   
+$("#tags").autocomplete({
+    source:searchApi
+})
+}); //end
+
+
+
+// footer
+let newDate = () => {
+    // JS DOM
+    document.getElementById("date_id").innerHTML = new Date().getFullYear();
+
+    // Jquery DOM
+    // $("#date_id").html(new Date().getFullYear());
+}
+newDate();
